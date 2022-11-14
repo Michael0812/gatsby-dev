66ceaad48510412097e50dccaf71de75c6086815
Michael0812 <michalwesolowski11@gmail.com>
2022-11-14 15:34:59 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Training`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
