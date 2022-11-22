a677a1a9fe889afd13b5016ed297a4032e4db6b8
Michael0812 <michalwesolowski11@gmail.com>
2022-11-22 22:47:13 +0000

    finished gatsby tutorial part2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+    <Layout pageTitle='About Us'>
+        <p>This is a random paragraph</p>
+    </Layout>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+    return (
+    <Layout pageHeaading='Contact - page heading' pageTitle='Contact Us - title'>
+        <p>this is a random paragraph</p>
+    </Layout>
+    )
+}
+
+export default ContactPage
